ca06b8e0dbd997a376b0c09e2059f1c5e93bb0ee
Miles Higbie <miles.higbie@gmail.com>
2020-03-01 21:24:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ class Square extends Polygon {
   
   get isValid() {
     if(this.array[0] === this.array[1] && this.array[1] === this.array[2] && this.array[2]=== this.array[3]){
+      return true
     }
   }
   
