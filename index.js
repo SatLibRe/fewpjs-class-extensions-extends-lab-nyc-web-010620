c1d7f9ac402123a8c1caa73805d39497e6f9249e
Miles Higbie <miles.higbie@gmail.com>
2020-03-01 21:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,12 @@ class Triangle extends Polygon {
 
 class Square extends Polygon {
   
+  get area(){
+    if (this.isValid === true){
+      this.array[0] * this.array[1]
+    }
+  }
+  
   get isValid(){
     if(this.array[0] === this.array[2] && this.array[1] === this.array[3] && this.array[0] === this.array[1]){
       return true;
